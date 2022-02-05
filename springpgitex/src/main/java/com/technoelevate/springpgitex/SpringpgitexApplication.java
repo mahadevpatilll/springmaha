b74da8c5bc7bc298f25d038bee8e5807159bb6c3
mahadev <mahadevpatil9035@gmail.com>
2022-02-05 12:17:15 +0530

    hjhjh

---
+++ springpgitex/src/main/java/com/technoelevate/springpgitex/SpringpgitexApplication.java
@@ -0,0 +1,13 @@
+package com.technoelevate.springpgitex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpgitexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpgitexApplication.class, args);
+	}
+
+}
---
+++ springpgitex/src/test/java/com/technoelevate/springpgitex/SpringpgitexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technoelevate.springpgitex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpgitexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
